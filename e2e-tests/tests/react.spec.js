65894580accf4fadd08427f1b5ec18363bc2fb0e
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2026-07-10 16:40:46 +0700

    fix: eslint error on test file

--- e2e-tests/tests/react.spec.js
+++ e2e-tests/tests/react.spec.js
@@ -1,9 +1,9 @@
-import { beforeEach, describe, expect, test } from "@playwright/test";
+import { describe, expect, test } from '@playwright/test'
 
 describe('Pokedex', () => {
-    test('Front page can be opened', async ({ page }) => {
-        await page.goto('http://localhost:5001')
-        await expect(page.getByText('ivysaur')).toBeVisible()
-        await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo')).toBeVisible()
-    })
+  test('Front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5001')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo')).toBeVisible()
+  })
 })
